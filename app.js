3ea2dfb56b8e5003779bab3bb487b495c9a1016a
Maria Smickersgill <mariasmickersgill@live.com>
2021-05-31 17:41:36 +0200

    Add handlebars files to project

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
+// Importing packages
 const express = require('express'); 
 const dotenv = require('dotenv'); 
+const path = require('path'); 
+const hbs = require('express-handlebars'); 
 const app = express(); 
 
+// Importing and setting up config
 dotenv.config({path: './config/config.env'});
 const PORT = process.env.PORT; 
 
+// For parsing request bodies
+app.use(express.json()); 
+
+// Set up the template engine
+// Describe the characteristics of the engine 
+app.engine('hbs', hbs({
+    defaultLayout: 'layout',
+    extname: '.hbs', 
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true, 
+        allowProtoMethodsByDefault: true
+    }
+})); 
+
+app.set('view engine', 'hbs'); 
+
+// Do I need this? 
+app.get('/', (req, res)=> {
+    res.render('/layout.hbs', {}); 
+})
+
+// Import & define routers 
 const routeUser = require('./routes/user'); 
 const routeNotes = require('./routes/notes'); 
 
+// Mount routers 
 app.use('/user', routeUser); 
 app.use('/notes', routeNotes); 
 
+// Static files
+const publicPath = path.resolve(__dirname,'public'); 
+app.use(express.static(publicPath)); 
+
+
+// Set up the server port listener 
 app.listen(PORT, ()=> {
     console.log(`Server running on http://localhost:${PORT}`);
 });
